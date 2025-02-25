5d57c2dcf64320953da4f53ab259ab484d859396
CallumNewman <ballumnew@gmail.com>
2025-02-25 20:55:04 +0000

    Added call to action button to the hero banner

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
+import MagicButton from "./ui/magic-button";
+import { FaLocationArrow } from "react-icons/fa6";
 
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
+
+      {/* Spotlight */}
       <div>
         <Spotlight
           className="-top-40 -left-10 md:left-32 md:-top-20 h-screen"
@@ -17,10 +21,12 @@ const Hero = () => {
         <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
       </div>
 
+      {/* Grid */}
       <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.3] flex items-center justify-center absolute top-0 left-0">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       </div>
+
 
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
@@ -28,11 +34,23 @@ const Hero = () => {
             Dynamic Web Magic With Next.js
           </h2>
 
-          <TextGenerateEffect className="text-center text-[40px] md:text-5xl lg:text-6xl" words="The Fifth Word is Purple"/>
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="The Fifth Word is Purple"
+          />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi, I&apos;m Callum, a full-stack developer based in the United Kingdom
+            Hi, I&apos;m Callum, a full-stack developer based in the United
+            Kingdom
           </p>
+
+          <a href="#about">
+            <MagicButton
+              title="View My Work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
         </div>
       </div>
     </div>
